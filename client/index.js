5b2d24fd672b07c8784e5eb65537f46563c505c5
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-02-04 11:12:46 +0000

    feat: added functionality to show more detail of a selected title when clicked
    'getTitle' fetches all data related to the titleId passed in and returns it
    
    'displayTitle' shows detailed information based on the titleId passed in

--- client/index.js
+++ client/index.js
@@ -1,5 +1,74 @@
 const el = {};
 
+/** returns information from a specific title */
+const getTitle = async (titleId) => {
+    try {
+        const params = titleId
+        if (!params) { return };
+
+        const response = await fetch(`https://api.imdbapi.dev/titles/${params}`)
+        const searchResult = await response.json();
+
+        const data = {
+            title: searchResult.primaryTitle,
+            type: searchResult.type,
+            startYear: searchResult.startYear,
+            endYear: searchResult.endYear,
+            genres: searchResult.genres,
+            rating: searchResult.rating?.aggregateRating ?? 'N/A',
+            plot: searchResult.plot,
+            thumbnail: searchResult.primaryImage?.url || ''
+        }
+
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// displays information from a specific title
+const displayTitle = async (id) => {
+    try {
+        const response = await getTitle(id);
+        if (!response) { return };
+
+        const div = document.createElement('div');
+        const title = document.createElement('p');
+        const type = document.createElement('p');
+        const startYear = document.createElement('p');
+        const endYear = document.createElement('p');
+        const genres = document.createElement('div');
+        const rating = document.createElement('p');
+        const plot = document.createElement('p');
+        const thumbnail = document.createElement('img');
+
+        title.textContent = response.title;
+        type.textContent = response.type;
+        startYear.textContent = response.startYear;
+        endYear.textContent = response.endYear;
+        // genres.textContent = 
+        rating.textContent = response.rating;
+        plot.textContent = response.plot;
+        thumbnail.src = response.thumbnail;
+        thumbnail.style.width = '200px';
+        thumbnail.style.height = '270px';
+
+        div.appendChild(title);
+        div.appendChild(type);
+        div.appendChild(startYear);
+        div.appendChild(endYear);
+        div.appendChild(rating);
+        div.appendChild(plot);
+        div.appendChild(thumbnail);
+
+        el.seriesTitle.appendChild(div);
+        
+    } catch (error) {
+        console.error('An error occured', error);
+    }
+}
+
 /** returns an array of titles from the imbd database that match the search query */
 const getMovies = async () => {
     try {
@@ -11,7 +80,7 @@ const getMovies = async () => {
         const response = await fetch(`https://api.imdbapi.dev/search/titles?query=${params}`);
         const searchResult = await response.json();
         console.log(searchResult.titles);
-
+        
         return searchResult.titles;
     } catch (error) {
         console.error('Error', error);
@@ -47,6 +116,11 @@ const displayMovies = async () => {
             div.appendChild(type);
             div.appendChild(rating);
             div.appendChild(thumbnail);
+
+            div.addEventListener('click', () => {
+                el.seriesTitle.textContent = '';
+                displayTitle(div.dataset.id)
+            })
 
             el.displayResults.appendChild(div);
         });
