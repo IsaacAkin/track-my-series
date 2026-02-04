a3b8948349053e9fcb11e2f87933fdfab92ceb5b
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-02-04 17:59:14 +0000

    feat: updated routes and views
    imported routes for index and watchlist pages
    
    set views and view engine
    
    updated asset path to allow for css and js file usage

--- server.js
+++ server.js
@@ -2,27 +2,30 @@ import 'dotenv/config.js';
 import path from 'node:path';
 import express from "express";
 import { fileURLToPath } from "node:url";
-import indexRouter from "./routes/index-routes.js";
-import collectionsRouter from "./routes/index-routes.js";
+import indexRouter from "./src/routes/index-routes.js";
+import watchlistRouter from "./src/routes/watchlist-routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const assetsPath = path.join(__dirname, 'client');
+const assetsPath = path.join(__dirname, 'src/public'); // for accessing css and js
 
+app.set('views', [
+    path.join(__dirname, 'src/views'),
+    path.join(__dirname, 'src/views/watchlist')
+]);
+app.set('view engine', 'ejs'); // views templete is set to use ejs
 app.use(express.static(assetsPath));
-// app.set('views', path.join(__dirname, 'client'));
 
 app.use('/', indexRouter);
-app.use('/watchlist', collectionsRouter);
+app.use('/watchlist', watchlistRouter);
 
 app.listen(PORT, (error) => {
     if (error) {
         console.error(error);
     }
 
-    // console.log(`Listening on port: ${PORT}`)
     console.log(`Listening on: http://localhost:${PORT}`)
 })
