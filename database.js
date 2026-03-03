2df78b50559107b53d628a8b0abd36d59912f8c5
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-03-03 12:45:01 +0000

    updated database functions and variables to reflect database structure changes
    changed the database model from having series stored in a collection of the watch status (e.g. title stored in plan-to-watch collection when it's added to the database) to a single collection called 'titles'.
    
    all variables and functions with the name 'collection' have been changes to 'status'

--- database.js
+++ database.js
@@ -2,52 +2,63 @@ import { MongoClient } from "mongodb";
 
 const uri = process.env.URI;
 const client = new MongoClient(uri);
+const trackMySeriesDB = 'track-my-series';
+const titlesCollection = 'titles';
 
 /** tests database connection to make sure everything is okay */
 const connectToDatabase = async () => {
     try {
-        await client.connect('track-my-series');
-        console.log(`Connected to the track-my-series database.`);
+        await client.connect(trackMySeriesDB);
+        console.log(`Connected to the ${trackMySeriesDB} database.`);
     } catch (err) {
         console.error(`Error connecting to database: ${err}`);
     }
 }
 
-/** adds a new series/movie document into the specified collection */
+/** adds a new series/movie document into the titles collection */
 export const addToCollection = async (id, title, type, startYear, endYear, plot, thumbnail) => {
     try {
         await connectToDatabase();
-        const planToWatch = client.db('track-my-series').collection('titles');
-        const query = { _id: id, title: title, type: type, startYear: startYear, endYear: endYear, plot: plot, thumbnail: thumbnail, status: 'plan to watch' };
+        const collection = client.db(trackMySeriesDB).collection(titlesCollection);
+        const query = { 
+            _id: id, 
+            title: title, 
+            type: type, 
+            startYear: startYear, 
+            endYear: endYear, 
+            plot: plot, 
+            thumbnail: thumbnail, 
+            status: 'plan-to-watch' 
+        };
 
-        const result = await planToWatch.insertOne(query);
-        console.log(`'${title}' has been added to the 'titles' collection with the _id: ${result.insertedId}.`);
+        const result = await collection.insertOne(query);
+        console.log(`'${title}' has been added to the '${titlesCollection}' collection with the _id: ${result.insertedId} and status of 'plan-to-watch'.`);
     } catch (error) {
-        console.error(error);
+        console.error('Error adding series to the collection:', error);
     } finally {
         await client.close();
     }
 }
 
-/** returns a populated array of all titles from a specified collection */
-export const getCollectionTitles = async (collectionName) => {
+/** returns a populated array of all series with the specified status from the titles collection */
+export const getTitlesWithStatus = async (status) => {
     const titles = [];
 
     try {
         await connectToDatabase();
-        const collection = client.db('track-my-series').collection(collectionName);
-        const query = collection.find();
+        const collection = client.db(trackMySeriesDB).collection(titlesCollection);
+        const query = collection.find({ status: status });
 
         for await (const title of query) {
             titles.push(title);
         }
 
         if (titles.length === 0) {
-            console.log(`No titles found in the '${collectionName}' collection.`);
+            console.log(`No titles found with the status of '${status}'.`);
             return titles;
         }
         
-        console.log(`${titles.length} titles found in the '${collectionName}' collection.`);
+        console.log(`${titles.length} titles found with the status of '${status}'.`);
         return titles;
     } catch (error) {
         console.error(error);
@@ -56,24 +67,24 @@ export const getCollectionTitles = async (collectionName) => {
     }
 }
 
-/** returns a single titles information based on the ID provided */
-export const getTitle = async (collectionName, titleId) => {
+/** returns the information of a single title in the database based on the ID provided */
+export const getTitle = async (titleId, status) => {
     try {
         await connectToDatabase();
-        const collection = client.db('track-my-series').collection(collectionName);
+        const collection = client.db(trackMySeriesDB).collection(titlesCollection);
         
-        const query = { _id: titleId }
+        const query = { _id: titleId, status: status }
         const title = await collection.findOne(query);
 
         if (!title) {
             return null;
         }
 
-        console.log(`Title with _id: ${titleId} found:`);
+        console.log(`Title with _id: ${titleId} and status: ${status} found:`);
         console.log(title);
         return title;
     } catch (error) {
-        console.error('Title _id could not be found in the collection:', error);
+        console.error(`Title with _id: ${titleId} and status: ${status} could not be found in the collection:`, error);
     } finally {
         client.close();
     }
