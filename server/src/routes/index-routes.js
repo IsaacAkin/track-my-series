aff69fbd53197f50137cf087cbd1d45aa400f2cb
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-05-18 22:57:02 +0100

    Removed all logic from index-routes and index-controllers
    As I currently dont have any functionality that I want to add to index, I have removed all logic from it
    
    I have an idea to have the homepage display the last 5 clicked items for each watchlist status and the
    last 5 clicked items for the search result. Until I implement that logic I will leave it empty.

--- server/src/routes/index-routes.js
+++ server/src/routes/index-routes.js
@@ -1,19 +1,6 @@
-import express from "express";
-import { addTitleToDatabase, getSeries, fetchTitleInfo } from "../controllers/index-controllers.js";
+// import express from "express";
+// import {} from "../controllers/index-controllers.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-export const watchlistLinks = [
-    { href: '/watchlist/watching', text: 'Watching' },
-    { href: '/watchlist/completed', text: 'Completed' },
-    { href: '/watchlist/on-hold', text: 'On Hold' },
-    { href: '/watchlist/plan-to-watch', text: 'Plan to Watch' },
-];
-
-router.get('/', (req, res) => res.render('index', { watchlistLinks }));
-router.get('/search', getSeries);
-router.get('/watchlist/title/:id', fetchTitleInfo);
-
-router.post('/title', addTitleToDatabase);
-
-export default router;
+// export default router;
