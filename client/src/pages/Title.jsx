b48dda1a56ed9a755838033e8d063381c284874c
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-05-26 23:38:39 +0100

    Created Title.jsx for displaying titles fetched from the database or API
    The component uses the id from the params to fetch title information.
    
    Database titles unique identifier field is '_id' whereas a title fetched from the API would just
    have and 'id' as its unique identifier. This is because the database titles are stored in a mongodb collection,
    where '_id' is what is used to give documents unique identifiers.
    
    When a title is fetched, it checks its unique identifier to know what to render on the screen.
    If it's being fetched from the API, then the rendered content will be different to the UI
    rendered by a title fetched from the database. Hence the creation of the ApiTitle and DatabaseTitle components.
    
    I am yet to add functionality for manipulating the database so for now it just displays information.

---
+++ client/src/pages/Title.jsx
@@ -0,0 +1,94 @@
+import Navbar from "../components/Navbar.jsx";
+import { useParams } from "react-router";
+import { useState, useEffect } from "react";
+
+function ApiTitle({ title }) {
+    return (
+        <div className="container">
+            <div className="title-information">
+                <div className="thumbnail-container">
+                    <img src={title.thumbnail} alt={title.name} className="full-thumbnail" />
+                </div>
+                <div className="example">
+                    <div className="context">
+                        <p className="primary-title">{title.title}</p>
+                        {title.type === 'tvSeries' && <p className="type">TV Series</p>}
+                        {title.type === 'movie' && <p className="type">Movie</p>}
+                        <p className="plot">{title.plot}</p>
+                    </div>
+                    <div className="status-buttons">
+                        <p>Rating: {title.rating}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function DatabaseTitle({ title }) {
+    return (
+        <div className="container">
+            <div className="title-information">
+                <div className="thumbnail-container">
+                    <img src={title.thumbnail} alt={title.name} className="full-thumbnail" />
+                </div>
+                <div className="example">
+                    <div className="context">
+                        <p className="primary-title">{title.title}</p>
+                        {title.type === 'tvSeries' && <p className="type">TV Series</p>}
+                        {title.type === 'movie' && <p className="type">Movie</p>}
+                        <p className="plot">{title.plot}</p>
+                    </div>
+                    <div className="status-buttons">
+                        <p>Rating: {title.rating}</p>
+                        <div className="episode-handler">
+                            <span id="total_episodes">{title.seasons[0].total_episodes}</span>
+                            <span>/</span>
+                            <span id="watched_episodes">{title.seasons[0].watched_episodes}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default function Title() {
+    const { id } = useParams();
+    const [title, setTitle] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchTitle() {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_API_URL}/title/${id}`);
+
+                if (!response.ok) {
+                    throw new Error(`Error ${response.status}`);
+                }
+
+                const data = await response.json();
+                setTitle(data.title);   
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchTitle();
+    }, [id])
+
+    return(
+        <div className="app">
+            <Navbar />
+            <>
+                {error && <p style={{ textAlign: 'center', color: 'white'}}>Error loading content</p>}
+                {loading && <p style={{ textAlign: 'center', color: 'white'}}>Loading...</p>}
+                { title && title.id && <ApiTitle title={title} /> }
+                { title && title._id && <DatabaseTitle title={title} /> }
+            </>
+        </div>
+    )
+}
