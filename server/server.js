2eee835e9792685ed83f195baab2bbf6ab394a21
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-07-28 18:08:44 +0100

    Added search route to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import cors from 'cors';
 import 'dotenv/config.js';
 import express from "express";
 // import indexRouter from "./src/routes/index-routes.js";
+import searchRouter from "./src/routes/search-routes.js";
 import titleRouter from "./src/routes/title-routes.js";
 import watchlistRouter from "./src/routes/watchlist-routes.js";
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json()); // to parse JSON data into req.body
 
 // app.use('/api', indexRouter);
+app.use('/api/search', searchRouter);
 app.use('/api/title', titleRouter);
 app.use('/api/watchlist', watchlistRouter);
 
