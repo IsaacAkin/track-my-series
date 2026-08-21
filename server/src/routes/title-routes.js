06df71aa2bef68e8fa062ec8575b7f3d56d3bc81
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-08-21 22:38:32 +0100

    updated function name in accordance with last commit
    updated 'changeEpisodeCount' function name to 'changeWatchedCount'

--- server/src/routes/title-routes.js
+++ server/src/routes/title-routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { changeTitleRating, changeTitleStatus, fetchTitle, addTitleToDatabase, deleteSingleTitle, changeEpisodeCount } from "../controllers/title-controllers.js";
+import { changeTitleRating, changeTitleStatus, fetchTitle, addTitleToDatabase, deleteSingleTitle, changeWatchedCount } from "../controllers/title-controllers.js";
 
 const router = express.Router();
 
@@ -9,7 +9,7 @@ router.post('/:mediaType/:id', addTitleToDatabase);
 
 router.patch('/:mediaType/:id/rating', changeTitleRating);
 router.patch('/:mediaType/:id/status', changeTitleStatus);
-router.patch('/:mediaType/:id/episodecount', changeEpisodeCount);
+router.patch('/:mediaType/:id/episodecount', changeWatchedCount);
 
 router.delete('/:mediaType/:id', deleteSingleTitle);
 
